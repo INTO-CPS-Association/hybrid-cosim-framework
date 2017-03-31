24b3af11dab2d99802d73fdaf5039a8ed31f3324
Kenneth Lausdahl <lausdahl@eng.au.dk>
2017-03-31 09:33:43 +0200

    added license and script to format code

--- window-sa/src/fmu.cpp
+++ window-sa/src/fmu.cpp
@@ -1,19 +1,31 @@
 /*
- * fmi.cpp
- *
- *  Created on: Aug 21, 2015
- *      Author: parallels
+ * #%~
+ * The Overture Abstract Syntax Tree
+ * %%
+ * Copyright (C) 2017 - 2014 Aarhus University
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #~%
  */
-
-#include<stdio.h>
-
 /*
  * fmi2Functions.c
  *
  *  Created on: May 22, 2015
  *      Author: kel
  */
-
+#include<stdio.h>
 #include "fmi2Functions.h"
 #include <string>
 
