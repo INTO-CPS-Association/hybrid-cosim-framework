fccd9917249a061ef98d33803f9e42c66b96db3c
Claudio Gomes <cla.gms@gmail.com>
2018-08-05 09:46:43 +0200

    flush in rules, and body out rules are run before each doStep

--- semantic-adaptation/src/SemanticAdaptation.h
+++ semantic-adaptation/src/SemanticAdaptation.h
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -378,6 +378,10 @@ double SemanticAdaptation<T>::do_step(shared_ptr<FmuComponent> fmuComp, double t
 #ifdef SA_DEBUG
 	printf("Calling doStep on %s,%p, comminicationPoint=%f, stepsize=%f\n",fmuComp->fmu->getPath()->c_str(),fmuComp->component,t,H);
 #endif
+
+	//flush all enabled in-rules
+	flushAllEnabledInRules();
+
 	fmi2Status status = fmuComp->fmu->doStep(fmuComp->component, t, H, false);
 	if (status != fmi2OK)
 	{
