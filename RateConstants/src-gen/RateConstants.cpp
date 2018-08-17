feb72e7c8aa221f06166eccae582f345c55dda09
Claudio Gomes <cla.gms@gmail.com>
2018-08-17 11:04:06 +0200

    added one more sample

--- RateConstants/src-gen/RateConstants.cpp
+++ RateConstants/src-gen/RateConstants.cpp
@@ -1,262 +1,233 @@
-			#include "RateConstants.h"
-										   
-   
-							   
-					
-				 
-				
+#include "RateConstants.h"
 
-					
-					 
-			
-			namespace adaptation 
+namespace adaptation 
+{
+	RateConstants::RateConstants(shared_ptr<std::string> fmiInstanceName,shared_ptr<string> resourceLocation, const fmi2CallbackFunctions* functions) : 
+	SemanticAdaptation(fmiInstanceName, resourceLocation, createInputRules(), createOutputRules(), functions)
+	{			
+		this->internalState.RATE = 10;
+		this->internalState.INIT_PSUVOLT = 0.0;
+		this->internalState.INIT_REF = 0.0;
+		this->internalState.stored__ref = this->internalState.INIT_REF;
+		this->internalState.stored__psuvolt = this->internalState.INIT_PSUVOLT;
+	}
+	
+	void RateConstants::initialize(bool loggingOn)
+	{
+		this->reactiveness = ReactiveOrDelayed::Reactive;
+		this->machineType = MooreOrMealy::Moore;
+		auto path1 = make_shared<string>(*resourceLocation);
+		path1->append(string("./Constants.fmu"));
+		auto constantsFmu = make_shared<fmi2::Fmu>(*path1);
+		constantsFmu->initialize();
+		this->constants = constantsFmu->instantiate("constants",fmi2CoSimulation, "{bb398d58-c0ba-4011-8fc2-a09893a75244}", true, loggingOn, shared_from_this());
+		
+		if(this->constants->component == NULL)
+			this->lastErrorState = fmi2Fatal;
+		this->instances->push_back(this->constants);
+	}
+	
+	RateConstants::~RateConstants()
+	{
+	}
+	
+	RateConstants* RateConstants::getRuleThis()
+	{
+		return this;
+	}
+	
+	double RateConstants::getFmiValueReal(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking double RateConstants::getFmiValueReal(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		switch (id)
+		{
+			case RATECONSTANTSPSUVOLT:
 			{
-				RateConstants::RateConstants(shared_ptr<std::string> fmiInstanceName,shared_ptr<string> resourceLocation, const fmi2CallbackFunctions* functions) : 
-				SemanticAdaptation(fmiInstanceName, resourceLocation, createInputRules(), createOutputRules(), functions)
-				{			
-					this->internalState.RATE = 10;
-					this->internalState.INIT_PSUVOLT = 0.0;
-					this->internalState.INIT_REF = 0.0;
-					this->internalState.stored__ref = this->internalState.INIT_REF;
-					this->internalState.stored__psuvolt = this->internalState.INIT_PSUVOLT;
-				}
-				
-				void RateConstants::initialize(bool loggingOn)
-				{
-					this->reactiveness = ReactiveOrDelayed::Reactive;
-					this->machineType = MooreOrMealy::Moore;
-					auto path1 = make_shared<string>(*resourceLocation);
-					path1->append(string("./Constants.fmu"));
-					auto constantsFmu = make_shared<fmi2::Fmu>(*path1);
-					constantsFmu->initialize();
-					this->constants = constantsFmu->instantiate("constants",fmi2CoSimulation, "{bb398d58-c0ba-4011-8fc2-a09893a75244}", true, loggingOn, shared_from_this());
-					
-					if(this->constants->component == NULL)
-						this->lastErrorState = fmi2Fatal;
-					this->instances->push_back(this->constants);
-				}
-				
-				RateConstants::~RateConstants()
-				{
-				}
-				
-				RateConstants* RateConstants::getRuleThis()
-				{
-					return this;
-				}
-				
-											double RateConstants::getFmiValueReal(fmi2ValueReference id)
-				
-																
-								  
-											{
-												#ifdef SA_DEBUG
-													printf("Invoking double RateConstants::getFmiValueReal(fmi2ValueReference id)");
-													printf("\n");
-												#endif	
-												switch (id)
-												{
-													case RATECONSTANTSPSUVOLT:
-													{
-														return this->internalState.psuvolt;
-													}
-													case RATECONSTANTSREF:
-													{
-														return this->internalState.ref;
-													}
-													default:
-													{
-														return 0.0;
-													}
-								  
-								
-						 
-											
-												}
-												
-											}
-				
-				string RateConstants::getFmiValueString(fmi2ValueReference id)
-				{
-					#ifdef SA_DEBUG
-						printf("Invoking string RateConstants::getFmiValueString(fmi2ValueReference id)");
-						printf("\n");
-					#endif	
-					return "";
-				}
-				
-				int RateConstants::getFmiValueInteger(fmi2ValueReference id)
-				{
-					#ifdef SA_DEBUG
-						printf("Invoking int RateConstants::getFmiValueInteger(fmi2ValueReference id)");
-						printf("\n");
-					#endif	
-					return 0;
-				}
-				
-				bool RateConstants::getFmiValueBoolean(fmi2ValueReference id)
-				{
-					#ifdef SA_DEBUG
-						printf("Invoking bool RateConstants::getFmiValueBoolean(fmi2ValueReference id)");
-						printf("\n");
-					#endif	
-					return false;
-				}
-				
-				void RateConstants::setFmiValue(fmi2ValueReference id, double value)
-				{
-					#ifdef SA_DEBUG
-						printf("RateConstants::setFmiValue(%d,?)",id);
-						printf("\n");
-					#endif	
-				}
-				
-				void RateConstants::setFmiValue(fmi2ValueReference id, string value)
-				{
-					#ifdef SA_DEBUG
-						printf("RateConstants::setFmiValue(%d,?)",id);
-						printf("\n");
-					#endif	
-				}
-				
-				void RateConstants::setFmiValue(fmi2ValueReference id, int value)
-				{
-					#ifdef SA_DEBUG
-						printf("RateConstants::setFmiValue(%d,?)",id);
-						printf("\n");
-					#endif	
-				}
-				
-				void RateConstants::setFmiValue(fmi2ValueReference id, bool value)
-				{
-					#ifdef SA_DEBUG
-						printf("RateConstants::setFmiValue(%d,?)",id);
-						printf("\n");
-					#endif	
-				}
-				
-				bool RateConstants::in_rule_condition1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking bool RateConstants::in_rule_condition1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-					return true;
-				}
-				void RateConstants::in_rule_body1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking void RateConstants::in_rule_body1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-				}
-				void RateConstants::in_rule_flush1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking void RateConstants::in_rule_flush1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-				}
-				shared_ptr<list<Rule<RateConstants>>> RateConstants::createInputRules()
-				{
-					auto list = make_shared<std::list<Rule<RateConstants>>>();
-					
-					list->push_back(
-						(Rule<RateConstants>){
-							&RateConstants::in_rule_condition1,
-							&RateConstants::in_rule_body1,
-							&RateConstants::in_rule_flush1
-						});
-					
-					
-					return list;
-					
-				}
-				
-				
-				double RateConstants::executeInternalControlFlow(double t, double H) {
-					#ifdef SA_DEBUG
-						printf("Invoking double RateConstants::executeInternalControlFlow(double t, double H)");
-						printf("\n");
-					#endif	
-					double micro_step = 0.0;
-					double inner_time = 0.0;
-					inner_time = t;
-					micro_step = H / this->internalState.RATE;
-					for (int iter = 0; iter<=this->internalState.RATE; iter++){
-						this->do_step(constants,inner_time,inner_time-t,micro_step);;
-						inner_time = inner_time + micro_step;
-					}
-					
-					return H;
-				}
-				
-				bool RateConstants::out_rule_condition1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking bool RateConstants::out_rule_condition1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-					return true;
-				}
-				void RateConstants::out_rule_body1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking void RateConstants::out_rule_body1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-					this->internalState.stored__ref = getValueDouble(constants,CONSTANTSREF);
-					this->internalState.stored__psuvolt = getValueDouble(constants,CONSTANTSPSUVOLT);
-				}
-				void RateConstants::out_rule_flush1(double dt, double H, double h){
-					#ifdef SA_DEBUG
-						printf("Invoking void RateConstants::out_rule_flush1(double dt, double H, double h)");
-						printf("\n");
-					#endif	
-					this->internalState.ref = this->internalState.stored__ref;
-					this->internalState.psuvolt = this->internalState.stored__psuvolt;
-				}
-				shared_ptr<list<Rule<RateConstants>>> RateConstants::createOutputRules()
-				{
-					auto list = make_shared<std::list<Rule<RateConstants>>>();
-					
-					list->push_back(
-						(Rule<RateConstants>){
-							&RateConstants::out_rule_condition1,
-							&RateConstants::out_rule_body1,
-							&RateConstants::out_rule_flush1
-						});
-					
-					
-					return list;
-					
-				}
-				
-				
-				fmi2FMUstate RateConstants::getInternalFMUState()
-				   
-							
-				{
-					InternalState* s = new InternalState();
-					*s = this->internalState;
-					return s;
-				}
-						
-					
-						
-					
-					   
-					
-						   
-					
-						
-					
-						  
-				
-				void RateConstants::setInternalFMUState(fmi2FMUstate state)
-				{
-					this->internalState = *(InternalState*)state;
-				}
-				
-				void RateConstants::freeInternalFMUState(fmi2FMUstate state)
-				{
-					delete (InternalState*)state;
-				}
-			
+				return this->internalState.psuvolt;
 			}
-			
+			case RATECONSTANTSREF:
+			{
+				return this->internalState.ref;
+			}
+			default:
+			{
+				return 0.0;
+			}
+		}
+	}
+	
+	string RateConstants::getFmiValueString(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking string RateConstants::getFmiValueString(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return "";
+	}
+	
+	int RateConstants::getFmiValueInteger(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking int RateConstants::getFmiValueInteger(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return 0;
+	}
+	
+	bool RateConstants::getFmiValueBoolean(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking bool RateConstants::getFmiValueBoolean(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return false;
+	}
+	
+	void RateConstants::setFmiValue(fmi2ValueReference id, double value)
+	{
+		#ifdef SA_DEBUG
+			printf("RateConstants::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	void RateConstants::setFmiValue(fmi2ValueReference id, string value)
+	{
+		#ifdef SA_DEBUG
+			printf("RateConstants::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	void RateConstants::setFmiValue(fmi2ValueReference id, int value)
+	{
+		#ifdef SA_DEBUG
+			printf("RateConstants::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	void RateConstants::setFmiValue(fmi2ValueReference id, bool value)
+	{
+		#ifdef SA_DEBUG
+			printf("RateConstants::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	bool RateConstants::in_rule_condition1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking bool RateConstants::in_rule_condition1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		return true;
+	}
+	void RateConstants::in_rule_body1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void RateConstants::in_rule_body1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+	}
+	void RateConstants::in_rule_flush1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void RateConstants::in_rule_flush1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+	}
+	shared_ptr<list<Rule<RateConstants>>> RateConstants::createInputRules()
+	{
+		auto list = make_shared<std::list<Rule<RateConstants>>>();
+		
+		list->push_back(
+			(Rule<RateConstants>){
+				&RateConstants::in_rule_condition1,
+				&RateConstants::in_rule_body1,
+				&RateConstants::in_rule_flush1
+			});
+		
+		
+		return list;
+		
+	}
+	
+	
+	double RateConstants::executeInternalControlFlow(double t, double H) {
+		#ifdef SA_DEBUG
+			printf("Invoking double RateConstants::executeInternalControlFlow(double t, double H)");
+			printf("\n");
+		#endif	
+		double micro_step = 0.0;
+		double inner_time = 0.0;
+		inner_time = t;
+		micro_step = H / this->internalState.RATE;
+		for (int iter = 0; iter<this->internalState.RATE; iter++){
+			this->do_step(constants,inner_time,inner_time-t,micro_step);;
+			inner_time = inner_time + micro_step;
+		}
+		
+		return H;
+	}
+	
+	bool RateConstants::out_rule_condition1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking bool RateConstants::out_rule_condition1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		return true;
+	}
+	void RateConstants::out_rule_body1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void RateConstants::out_rule_body1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		this->internalState.stored__ref = getValueDouble(constants,CONSTANTSREF);
+		this->internalState.stored__psuvolt = getValueDouble(constants,CONSTANTSPSUVOLT);
+	}
+	void RateConstants::out_rule_flush1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void RateConstants::out_rule_flush1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		this->internalState.ref = this->internalState.stored__ref;
+		this->internalState.psuvolt = this->internalState.stored__psuvolt;
+	}
+	shared_ptr<list<Rule<RateConstants>>> RateConstants::createOutputRules()
+	{
+		auto list = make_shared<std::list<Rule<RateConstants>>>();
+		
+		list->push_back(
+			(Rule<RateConstants>){
+				&RateConstants::out_rule_condition1,
+				&RateConstants::out_rule_body1,
+				&RateConstants::out_rule_flush1
+			});
+		
+		
+		return list;
+		
+	}
+	
+	
+	fmi2FMUstate RateConstants::getInternalFMUState()
+				
+	{
+		InternalState* s = new InternalState();
+		*s = this->internalState;
+		return s;
+	}
+	
+	void RateConstants::setInternalFMUState(fmi2FMUstate state)
+	{
+		this->internalState = *(InternalState*)state;
+	}
+	
+	void RateConstants::freeInternalFMUState(fmi2FMUstate state)
+	{
+		delete (InternalState*)state;
+	}
+
+}
+
---
+++ semantic_adaptation_samples/PlantMultiRate/src-gen/PlantMultiRate.cpp
@@ -0,0 +1,307 @@
+#include "PlantMultiRate.h"
+
+namespace adaptation 
+{
+	PlantMultiRate::PlantMultiRate(shared_ptr<std::string> fmiInstanceName,shared_ptr<string> resourceLocation, const fmi2CallbackFunctions* functions) : 
+	SemanticAdaptation(fmiInstanceName, resourceLocation, createInputRules(), createOutputRules(), functions)
+	{			
+		this->internalState.RATE = 10;
+		this->internalState.INIT_TORQUE = 0.0;
+		this->internalState.INIT_PSU = 0.0;
+		this->internalState.INIT_V = 0.0;
+		this->internalState.INIT_X = 0.0;
+		this->internalState.INIT_W = 0.0;
+		this->internalState.INIT_F = 0.0;
+		this->internalState.current_torque = 0.0;
+		this->internalState.current_psu = 0.0;
+		this->internalState.current_v = 0.0;
+		this->internalState.current_x = 0.0;
+		this->internalState.stored__psu = this->internalState.INIT_PSU;
+		this->internalState.stored__x = this->internalState.INIT_X;
+		this->internalState.stored__v = this->internalState.INIT_V;
+		this->internalState.stored__torque = this->internalState.INIT_TORQUE;
+		this->internalState.stored__w = this->internalState.INIT_W;
+		this->internalState.stored__f = this->internalState.INIT_F;
+		this->internalState.previous_torque = 0.0;
+		this->internalState.previous_psu = 0.0;
+		this->internalState.previous_v = 0.0;
+		this->internalState.previous_x = 0.0;
+	}
+	
+	void PlantMultiRate::initialize(bool loggingOn)
+	{
+		this->reactiveness = ReactiveOrDelayed::Reactive;
+		this->machineType = MooreOrMealy::Moore;
+		auto path1 = make_shared<string>(*resourceLocation);
+		path1->append(string("./EMAPlantNoLoad_FixedEuler_1Em6.fmu"));
+		auto plantFmu = make_shared<fmi2::Fmu>(*path1);
+		plantFmu->initialize();
+		this->plant = plantFmu->instantiate("plant",fmi2CoSimulation, "698567049", true, loggingOn, shared_from_this());
+		
+		if(this->plant->component == NULL)
+			this->lastErrorState = fmi2Fatal;
+		this->instances->push_back(this->plant);
+	}
+	
+	PlantMultiRate::~PlantMultiRate()
+	{
+	}
+	
+	PlantMultiRate* PlantMultiRate::getRuleThis()
+	{
+		return this;
+	}
+	
+	double PlantMultiRate::getFmiValueReal(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking double PlantMultiRate::getFmiValueReal(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		switch (id)
+		{
+			case PLANTMULTIRATEW:
+			{
+				return this->internalState.w;
+			}
+			case PLANTMULTIRATEF:
+			{
+				return this->internalState.f;
+			}
+			default:
+			{
+				return 0.0;
+			}
+		}
+	}
+	
+	string PlantMultiRate::getFmiValueString(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking string PlantMultiRate::getFmiValueString(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return "";
+	}
+	
+	int PlantMultiRate::getFmiValueInteger(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking int PlantMultiRate::getFmiValueInteger(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return 0;
+	}
+	
+	bool PlantMultiRate::getFmiValueBoolean(fmi2ValueReference id)
+	{
+		#ifdef SA_DEBUG
+			printf("Invoking bool PlantMultiRate::getFmiValueBoolean(fmi2ValueReference id)");
+			printf("\n");
+		#endif	
+		return false;
+	}
+	
+	void PlantMultiRate::setFmiValue(fmi2ValueReference id, double value)
+	{
+		#ifdef SA_DEBUG
+			printf("PlantMultiRate::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+		switch (id)	
+			{
+				case PLANTMULTIRATETORQUE:
+				{
+					this->internalState.torque = value;
+					this->internalState.isSettorque = true;
+					break;
+				}
+				case PLANTMULTIRATEPSU:
+				{
+					this->internalState.psu = value;
+					this->internalState.isSetpsu = true;
+					break;
+				}
+				case PLANTMULTIRATEV:
+				{
+					this->internalState.v = value;
+					this->internalState.isSetv = true;
+					break;
+				}
+				case PLANTMULTIRATEX:
+				{
+					this->internalState.x = value;
+					this->internalState.isSetx = true;
+					break;
+				}
+				default:
+				{
+				}
+			}
+	}
+	
+	void PlantMultiRate::setFmiValue(fmi2ValueReference id, string value)
+	{
+		#ifdef SA_DEBUG
+			printf("PlantMultiRate::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	void PlantMultiRate::setFmiValue(fmi2ValueReference id, int value)
+	{
+		#ifdef SA_DEBUG
+			printf("PlantMultiRate::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	void PlantMultiRate::setFmiValue(fmi2ValueReference id, bool value)
+	{
+		#ifdef SA_DEBUG
+			printf("PlantMultiRate::setFmiValue(%d,?)",id);
+			printf("\n");
+		#endif	
+	}
+	
+	bool PlantMultiRate::in_rule_condition1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking bool PlantMultiRate::in_rule_condition1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		return true;
+	}
+	void PlantMultiRate::in_rule_body1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void PlantMultiRate::in_rule_body1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		this->internalState.stored__v = this->internalState.v;
+		this->internalState.stored__psu = this->internalState.psu;
+		this->internalState.stored__torque = this->internalState.torque;
+		this->internalState.stored__x = this->internalState.x;
+		this->internalState.current_torque = this->internalState.torque;
+		this->internalState.current_psu = this->internalState.psu;
+		this->internalState.current_x = this->internalState.x;
+		this->internalState.current_v = this->internalState.v;
+	}
+	void PlantMultiRate::in_rule_flush1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void PlantMultiRate::in_rule_flush1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		setValue(plant,PLANTX,this->internalState.stored__x);
+		;
+		setValue(plant,PLANTTORQUE,this->internalState.stored__torque);
+		;
+		setValue(plant,PLANTV,this->internalState.stored__v);
+		;
+		setValue(plant,PLANTPSU,this->internalState.stored__psu);
+		;
+		setValue(plant,PLANTTORQUE,this->internalState.previous_torque + (((this->internalState.current_torque - this->internalState.previous_torque) / H) * dt));
+		;
+		setValue(plant,PLANTPSU,this->internalState.previous_psu + (((this->internalState.current_psu - this->internalState.previous_psu) / H) * dt));
+		;
+		setValue(plant,PLANTX,this->internalState.previous_x + (((this->internalState.current_x - this->internalState.previous_x) / H) * dt));
+		;
+		setValue(plant,PLANTV,this->internalState.previous_v + (((this->internalState.current_v - this->internalState.previous_v) / H) * dt));
+		;
+	}
+	shared_ptr<list<Rule<PlantMultiRate>>> PlantMultiRate::createInputRules()
+	{
+		auto list = make_shared<std::list<Rule<PlantMultiRate>>>();
+		
+		list->push_back(
+			(Rule<PlantMultiRate>){
+				&PlantMultiRate::in_rule_condition1,
+				&PlantMultiRate::in_rule_body1,
+				&PlantMultiRate::in_rule_flush1
+			});
+		
+		
+		return list;
+		
+	}
+	
+	
+	double PlantMultiRate::executeInternalControlFlow(double t, double H) {
+		#ifdef SA_DEBUG
+			printf("Invoking double PlantMultiRate::executeInternalControlFlow(double t, double H)");
+			printf("\n");
+		#endif	
+		double micro_step = 0.0;
+		double inner_time = 0.0;
+		inner_time = t;
+		micro_step = H / this->internalState.RATE;
+		for (int iter = 0; iter<this->internalState.RATE; iter++){
+			this->do_step(plant,inner_time,inner_time-t,micro_step);;
+			inner_time = inner_time + micro_step;
+		}
+		this->internalState.previous_torque = this->internalState.current_torque;
+		this->internalState.previous_psu = this->internalState.current_psu;
+		this->internalState.previous_v = this->internalState.current_v;
+		this->internalState.previous_x = this->internalState.current_x;
+		
+		return H;
+	}
+	
+	bool PlantMultiRate::out_rule_condition1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking bool PlantMultiRate::out_rule_condition1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		return true;
+	}
+	void PlantMultiRate::out_rule_body1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void PlantMultiRate::out_rule_body1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		this->internalState.stored__w = getValueDouble(plant,PLANTW);
+		this->internalState.stored__f = getValueDouble(plant,PLANTF);
+	}
+	void PlantMultiRate::out_rule_flush1(double dt, double H, double h){
+		#ifdef SA_DEBUG
+			printf("Invoking void PlantMultiRate::out_rule_flush1(double dt, double H, double h)");
+			printf("\n");
+		#endif	
+		this->internalState.w = this->internalState.stored__w;
+		this->internalState.f = this->internalState.stored__f;
+	}
+	shared_ptr<list<Rule<PlantMultiRate>>> PlantMultiRate::createOutputRules()
+	{
+		auto list = make_shared<std::list<Rule<PlantMultiRate>>>();
+		
+		list->push_back(
+			(Rule<PlantMultiRate>){
+				&PlantMultiRate::out_rule_condition1,
+				&PlantMultiRate::out_rule_body1,
+				&PlantMultiRate::out_rule_flush1
+			});
+		
+		
+		return list;
+		
+	}
+	
+	
+	fmi2FMUstate PlantMultiRate::getInternalFMUState()
+				
+	{
+		InternalState* s = new InternalState();
+		*s = this->internalState;
+		return s;
+	}
+	
+	void PlantMultiRate::setInternalFMUState(fmi2FMUstate state)
+	{
+		this->internalState = *(InternalState*)state;
+	}
+	
+	void PlantMultiRate::freeInternalFMUState(fmi2FMUstate state)
+	{
+		delete (InternalState*)state;
+	}
+
+}
+
